ccb418d2ddd2c04483bfaba1acb706d9f342ba9e
Farkas Péter <farkas.peter1995@gmail.com>
2015-06-15 17:35:40 +0200

    Exception log on System.out

--- src/main/java/hu/ptemik/gallery/hibernate/HibernateUtil.java
+++ src/main/java/hu/ptemik/gallery/hibernate/HibernateUtil.java
@@ -18,23 +18,23 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
-    
+
     static {
         try {
             // Create the SessionFactory from standard (hibernate.cfg.xml) 
             // config file.
             Configuration configuration = new Configuration().configure();
-        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().
-                applySettings(configuration.getProperties());
-        
-        sessionFactory = configuration.buildSessionFactory(builder.build()); 
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().
+                    applySettings(configuration.getProperties());
+
+            sessionFactory = configuration.buildSessionFactory(builder.build());
         } catch (Throwable ex) {
             // Log the exception. 
-            System.err.println("Initial SessionFactory creation failed." + ex);
+            System.out.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
-    
+
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
